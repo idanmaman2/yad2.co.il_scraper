71b70d561b2c105b38d2537709b2453b2658ea12
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2022-10-21 11:49:04 +0300

    refactoring  last version

--- yad2scraper.py
+++ yad2scraper.py
@@ -1,13 +1,14 @@
 LinkTemplate = """https://gw.yad2.co.il/feed-search-legacy/products/%s?category=%d&item=%d&page=%d&forceLdLoad=true"""
 import requests 
 import csv
+import json
 def fetchJson(section : str  , catgeory : int, item : int , printIt= False , page : int =0):
     '''generator to get all yad2 item and category pages and results '''
     jsonRes = requests.get(LinkTemplate%( section , catgeory , item , page )).json()
     for itemJson in jsonRes["data"]["feed"]["feed_items"]: 
-        yield itemJson 
-        if printIt : 
-            print(f"""price : {itemJson["price"] if "price" in itemJson else "Unknown" } , area : {itemJson["SecondaryArea"] + " : " + str(itemJson["area_id"])  if "SecondaryArea" in itemJson else "Unknown"} """)
+        yield itemJson
+        if printIt :
+            print(json.dumps(itemJson,ensure_ascii=False))
     if(jsonRes["data"]["pagination"]["current_page"] < jsonRes["data"]["pagination"]["last_page"]  ): 
         yield from fetchJson(section , catgeory , item  , printIt, page+1)
 
@@ -22,10 +23,7 @@ def to_csv(name, jsonList : list ):
         for index,item in enumerate(jsonList) : 
             if not(index):
                   spamwriter.writerow(item.keys())
-            values = [] 
-            for  value in item.values():
-                    values.append(str(value).replace("\t",""))
-            spamwriter.writerow(values)
-to_csv("items_in_section.csv" ,items("furniture" , 2,"item") )     
-to_csv("areas_codes.csv" ,items("furniture" , 2,"area") )    
-to_csv("fetched_data.csv" ,fetchJson("furniture" , 2,320,True) )
+            spamwriter.writerow(list(map(lambda val : str(val).replace("\t","     "),item.values())))
+to_csv("items_in_section.csv" ,items("cellular" , 5,"item") )     
+to_csv("areas_codes.csv" ,items("cellular" , 5,"area") )    
+to_csv("fetched_data.csv" ,fetchJson("cellular" , 5,29,True) )
