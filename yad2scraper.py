43f2cf7e1f2f258f389125f03c83f02afca60e1d
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-01-26 00:56:16 +0200

    refactoring

--- yad2scraper.py
+++ yad2scraper.py
@@ -2,28 +2,28 @@ LinkTemplate = """https://gw.yad2.co.il/feed-search-legacy/products/%s?category=
 import requests 
 import csv
 import json
-def fetchJson(section : str  , catgeory : int, item : int , printIt= False , page : int =0):
+from itertools import chain
+def fetch_json(section : str  , catgeory : int, item : int , printIt= False , page : int =0):
     '''generator to get all yad2 item and category pages and results '''
     jsonRes = requests.get(LinkTemplate%( section , catgeory , item , page )).json()
     for itemJson in jsonRes["data"]["feed"]["feed_items"]: 
         yield itemJson
         if printIt :
             print(json.dumps(itemJson,ensure_ascii=False))
-    if(jsonRes["data"]["pagination"]["current_page"] < jsonRes["data"]["pagination"]["last_page"]  ): 
-        yield from fetchJson(section , catgeory , item  , printIt, page+1)
+    if jsonRes["data"]["pagination"]["current_page"] < jsonRes["data"]["pagination"]["last_page"]: 
+        yield from fetch_json(section , catgeory , item  , printIt, page+1)
 
 def items(section , catgeory : int , searchTerm):
     cats = requests.get(f"https://gw.yad2.co.il/search-options/products/{section}?fields={searchTerm}&category={catgeory}").json()
     yield from cats["data"][searchTerm] 
 
-    
 def to_csv(name, jsonList : list ):
     with open(name, 'w', newline='',encoding="utf-16") as csvfile:
         spamwriter = csv.writer(csvfile, delimiter='\t')
-        for index,item in enumerate(jsonList) : 
-            if not(index):
-                  spamwriter.writerow(item.keys())
-            spamwriter.writerow(list(map(lambda val : str(val).replace("\t","     "),item.values())))
+        first = next(jsonList)
+        spamwriter.writerow(first.keys() )
+        for item in chain(jsonList,(first,)):
+            spamwriter.writerow(map(lambda val : str(val).replace("\t","     "),item.values()))
 to_csv("items_in_section.csv" ,items("cellular" , 5,"item") )     
 to_csv("areas_codes.csv" ,items("cellular" , 5,"area") )    
-to_csv("fetched_data.csv" ,fetchJson("cellular" , 5,29,True) )
+to_csv("fetched_data.csv" ,fetch_json("cellular" , 5,29,True) )
